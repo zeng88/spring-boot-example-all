b434e96cf72e6128ff1cf3d5bdba383974bcd4cc
谢帅 <xieshuai@finupgroup.com>
2020-01-06 15:32:28 +0800

    change rabbitmq multiple

--- spring-boot-rabbitmq-multiple/src/main/java/com/example/config/rabbitmq/MyRabbitMQCreateConfig.java
+++ spring-boot-rabbitmq-multiple/src/main/java/com/example/config/rabbitmq/MyRabbitMQCreateConfig.java
@@ -27,11 +27,11 @@ public class MyRabbitMQCreateConfig {
 
 
 //        v2RabbitAdmin.declareExchange(new DirectExchange("test.direct", false, false));
-        v2RabbitAdmin.declareExchange(new TopicExchange("exchange.topic.example.new", false, false));
+        v2RabbitAdmin.declareExchange(new TopicExchange("exchange.topic.example.new", true, false));
 //        v2RabbitAdmin.declareExchange(new FanoutExchange("test.fanout", false, false));
 
 //        v2RabbitAdmin.declareQueue(new Queue("test.direct.queue", false));
-        v2RabbitAdmin.declareQueue(new Queue("queue.example.topic.new", false));
+        v2RabbitAdmin.declareQueue(new Queue("queue.example.topic.new", true));
 //        v2RabbitAdmin.declareQueue(new Queue("test.fanout.queue", false));
 
 //        v2RabbitAdmin.declareBinding(new Binding("test.direct.queue",
@@ -40,8 +40,8 @@ public class MyRabbitMQCreateConfig {
 
         v2RabbitAdmin.declareBinding(
                 BindingBuilder
-                        .bind(new Queue("queue.example.topic.new", false))        //直接创建队列
-                        .to(new TopicExchange("exchange.topic.example.new", false, false))    //直接创建交换机 建立关联关系
+                        .bind(new Queue("queue.example.topic.new", true))        //直接创建队列
+                        .to(new TopicExchange("exchange.topic.example.new", true, false))    //直接创建交换机 建立关联关系
                         .with("routing.key.example.new"));    //指定路由Key
 
 //        v2RabbitAdmin.declareBinding(
--- spring-boot-rabbitmq-multiple/src/main/java/com/example/topic/TopicProducer.java
+++ spring-boot-rabbitmq-multiple/src/main/java/com/example/topic/TopicProducer.java
@@ -12,12 +12,21 @@ public class TopicProducer {
     @Resource(name = "v1RabbitTemplate")
     private RabbitTemplate v1RabbitTemplate;
 
+    @Resource(name = "v2RabbitTemplate")
+    private RabbitTemplate v2RabbitTemplate;
+
     public void sendMessageByTopic() {
-        String content = "This is a topic type of the RabbitMQ message example";
-        this.v1RabbitTemplate.convertAndSend(
+        String content1 = "This is a topic type of the RabbitMQ message example from v1RabbitTemplate";
+        v1RabbitTemplate.convertAndSend(
                 "exchange.topic.example.new",
                 "routing.key.example.new",
-                content);
+                content1);
+
+        String content2 = "This is a topic type of the RabbitMQ message example from v2RabbitTemplate";
+        v2RabbitTemplate.convertAndSend(
+                "exchange.topic.example.new",
+                "routing.key.example.new",
+                content2);
     }
 
 }
